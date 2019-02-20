aa708c0a49f252f967a6f2cedd789654ea6e8723
simaos <36926049+simaos@users.noreply.github.com>
2019-02-20 22:09:34 +0530

    Add static analysis

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,2 +1,24 @@
-Param([Parameter(Mandatory=$true)][String]$DockerTarget)
+Param([Switch]$AppVeyor,[Parameter(Mandatory=$true)][String]$DockerTarget)
+
+$TestName = "PSScriptAnalyzerShouldHaveZeroFindings"
+If ($AppVeyor.IsPresent) {
+    Add-AppveyorTest -Name $TestName -Outcome Running
+}
+Write-Information "$TestName Running"
+
+$Results = Invoke-ScriptAnalyzer -Path $pwd -Recurse -ErrorAction SilentlyContinue
+If ($Results) {
+    $ResultString = $Results | Out-String
+    Write-Warning $ResultString
+    If ($AppVeyor.IsPresent) { 
+        Add-AppveyorMessage -Message "$TestName failed. See test results for more details" -Category Error
+        Update-AppveyorTest -Name "$TestName" -Outcome Failed -ErrorMessage $ResultString
+    }
+    Throw "Fail build: $TestName has findings"
+}
+Else {
+    Write-Information "$TestName passed"
+    Update-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Passed
+}
+
 docker run $DockerTarget PowerShell -Command "Write-Host 'Pester is working if the next tests fails' ; New-Fixture -Path . -Name Invoke-IShouldFail; Invoke-Pester ./Invoke-IShouldFail* ; Write-Host 'PSScriptAnalyzer is working if it reports 0 rule violations found. Running Invoke-ScriptAnalyzer...' ; Invoke-ScriptAnalyzer -Path . -ReportSummary"
