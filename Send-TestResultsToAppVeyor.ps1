c5584520be517f4bbad64bd962a26612e6fd84ce
simaos <36926049+simaos@users.noreply.github.com>
2019-02-28 16:58:33 +0000

    move appveyor upload of test results to appveyor config

--- Send-TestResultsToAppVeyor.ps1
+++ Send-TestResultsToAppVeyor.ps1
@@ -1,4 +1,6 @@
 #Requires -Version 5.0
 $url="https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)"
+Write-Output "Uploading test results to $url"
 $path=Resolve-Path .\FullTestResults.xml
+Write-Output "Uploading test results from $path"
 (New-Object 'System.Net.WebClient').UploadFile($url, $path)
